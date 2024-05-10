85f10aa829f1a2a7a4eb06655704d7f92145f942
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2024-05-10 17:24:16 +0800

    fix case url

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -8,7 +8,7 @@ function Root() {
                 <ul>
                     <li><a href={`/#/case/1`}>Test case 1</a></li>
                     <li><a href={`/#/case/2`}>Test case 2</a></li>
-                    <li><a href={`/#/case/3`}>Test case 2</a></li>
+                    <li><a href={`/#/case/3`}>Test case 3</a></li>
                 </ul>
             </nav>
         </div>

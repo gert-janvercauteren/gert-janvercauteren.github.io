31088e97d9e16b6eda7db563b35d9c7628dfff47
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2024-07-03 21:33:06 +0800

    update flight card

--- src/components/DesktopFlightCard/DesktopFlightCard.tsx
+++ src/components/DesktopFlightCard/DesktopFlightCard.tsx
@@ -3,7 +3,7 @@ import BpkText, {TEXT_STYLES} from "@skyscanner/backpack-web/bpk-component-text"
 import React, {useRef} from "react";
 import './DesktopFlightCard.scss'
 import {BpkDividedCard} from "@skyscanner/backpack-web/bpk-component-card";
-import BpkTooltip from "@skyscanner/backpack-web/bpk-component-tooltip/src/BpkTooltip";
+import BpkTooltip from "@skyscanner/backpack-web/bpk-component-tooltip";
 
 interface Props {
     option: number
@@ -20,16 +20,40 @@ function DesktopFlightCard({title, price, stars, rating, reviews, option, cityCe
     const outboundA11y = "Outbound, Swiss airlines, from London Heathrow at 12:30 to Barcelona at 14:30, Direct flight, 2 hours 20 minutes"
     const returnA11y = "Return, Swiss airlines, from Barcelona at 12:30 to London Heathrow at 14:30, Direct flight, 2 hours 20 minutes"
 
-    const targetRef = useRef(null);
-    const target = (
-        <div ref={targetRef} className={'my-tooltip-target'}>
-            <BpkText textStyle="lg">LHR</BpkText>
+    const targetRefOutboundOutbound = useRef(null);
+    const targetOutboundOutbound = (
+        <div ref={targetRefOutboundOutbound} className={'my-tooltip-target'}>
+            <BpkText>LHR</BpkText>
+        </div>
+    );
+
+    const targetRefOutboundArrival = useRef(null);
+    const targetOutboundArrival = (
+        <div ref={targetRefOutboundArrival} className={'my-tooltip-target'}>
+            <BpkText>BCN</BpkText>
+        </div>
+    );
+
+    const targetRefReturnArrival = useRef(null);
+    const targetReturnArrival = (
+        <div ref={targetRefReturnArrival} className={'my-tooltip-target'}>
+            <BpkText>LHR</BpkText>
+        </div>
+    );
+
+    const targetRefReturnOutbound = useRef(null);
+    const targetReturnOutbound = (
+        <div ref={targetRefReturnOutbound} className={'my-tooltip-target'}>
+            <BpkText>BCN</BpkText>
         </div>
     );
 
     const mainContent = (
         <div className={"FlightLegContainer"}>
-            <BpkText tagName={'h2'} className={'visually-hidden'}>Swiss airlines, Option 1</BpkText>
+            <BpkText tagName={'h2'}>
+                <a href={"https://example.com"} className={"FlightCard__CardLink"}>
+                    <span className={'visually-hidden'}>Swiss airlines, Option 1</span></a>
+            </BpkText>
 
             {/* Outbound leg */}
             <div aria-label={outboundA11y}>
@@ -39,7 +63,13 @@ function DesktopFlightCard({title, price, stars, rating, reviews, option, cityCe
                     </div>
                     <div className={"FlightLeg__Outbound"}>
                         <BpkText textStyle={TEXT_STYLES.heading4}>12:30</BpkText>
-                        <BpkText>LHR</BpkText>
+                        <BpkTooltip
+                            ariaLabel="London Heathrow"
+                            id="my-tooltip"
+                            target={targetOutboundArrival}
+                        >
+                            London Heathrow
+                        </BpkTooltip>
                     </div>
                     <div className={"FlightLeg__Duration"}>
                         <BpkText>02h20</BpkText>
@@ -47,7 +77,13 @@ function DesktopFlightCard({title, price, stars, rating, reviews, option, cityCe
                     </div>
                     <div className={"FlightLeg__Inbound"}>
                         <BpkText textStyle={TEXT_STYLES.heading4}>14:50</BpkText>
-                        <BpkText>BCN</BpkText>
+                        <BpkTooltip
+                            ariaLabel="London Heathrow"
+                            id="my-tooltip"
+                            target={targetOutboundOutbound}
+                        >
+                            Barcelona
+                        </BpkTooltip>
                     </div>
                 </div>
             </div>
@@ -59,7 +95,13 @@ function DesktopFlightCard({title, price, stars, rating, reviews, option, cityCe
                     </div>
                     <div className={"FlightLeg__Outbound"}>
                         <BpkText textStyle={TEXT_STYLES.heading4}>12:30</BpkText>
-                        <BpkText>BCN</BpkText>
+                        <BpkTooltip
+                            ariaLabel="London Heathrow"
+                            id="my-tooltip"
+                            target={targetReturnOutbound}
+                        >
+                            Barcelona
+                        </BpkTooltip>
                     </div>
                     <div className={"FlightLeg__Duration"}>
                         <BpkText>02h20</BpkText>
@@ -70,7 +112,7 @@ function DesktopFlightCard({title, price, stars, rating, reviews, option, cityCe
                         <BpkTooltip
                             ariaLabel="London Heathrow"
                             id="my-tooltip"
-                            target={target}
+                            target={targetReturnArrival}
                         >
                             London Heathrow
                         </BpkTooltip>

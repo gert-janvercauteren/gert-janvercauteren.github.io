7bf1b0915be7eb5c17c16b9c0aa62cafbcb2c60c
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2025-02-28 17:25:11 +0800

    expand list demo

--- src/routes/ListDemo.tsx
+++ src/routes/ListDemo.tsx
@@ -6,6 +6,26 @@ import './ListDemo.scss'
 function ListDemo() {
     return (
         <div className='main-content'>
+            <BpkText textStyle={TEXT_STYLES.heading2} tagName='h1'>Lists</BpkText>
+            <BpkText textStyle={TEXT_STYLES.heading2} tagName='h2'>Using paragraphs</BpkText>
+            <ul>
+                <li>
+                    {/* eslint-disable-next-line jsx-a11y/aria-role */}
+                    <p className='list-item' role="text">
+                        <BpkText textStyle={TEXT_STYLES.label2} tagName='span'>Free cancellation</BpkText>
+                        <BpkText textStyle={TEXT_STYLES.footnote} tagName='span'>No</BpkText>
+                    </p>
+                </li>
+                <li>
+                    {/* eslint-disable-next-line jsx-a11y/aria-role */}
+                    <p className='list-item' role="text">
+                        <BpkText textStyle={TEXT_STYLES.label2} tagName='span'>Mileage policy</BpkText>
+                        <BpkText textStyle={TEXT_STYLES.footnote} tagName='span'>200 miles included</BpkText>
+                    </p>
+                </li>
+            </ul>
+
+            <BpkText textStyle={TEXT_STYLES.heading2} tagName='h2'>Using VoiceOver 'hack'</BpkText>
             <ul>
                 <li>
                     {/* eslint-disable-next-line jsx-a11y/aria-role */}
